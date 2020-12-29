cc5dc8a1a608b7f65a279b7bd1b14e9fa868e1d9
Likhith Aalla <likhithaalla@Likhiths-MacBook-Pro.local>
2020-12-29 14:38:26 -0600

    Initial Commit

---
+++ build/static/js/53.912f9904.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreactcap=this.webpackJsonpreactcap||[]).push([[53],{136:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var c=e(11),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(c.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var i=o.closest("ion-content");i&&i.componentOnReady().then((function(){Object(c.f)((function(){return i.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.912f9904.chunk.js.map
