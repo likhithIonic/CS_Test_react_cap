4ab22bf0c33831963f16e5321332b3cbaee3a675
kimberly McIntyre <kimberlymcintyre@kimberlys-MacBook-Air.local>
2022-03-07 09:42:31 -0500

    Updated resources for latest React template

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,16 +1,10 @@
-/// <reference types="@capacitor/splash-screen" />
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'io.ionic.starter',
-    appName: 'reactcap',
-    webDir: 'build',
-    plugins: {
-        SplashScreen: {
-            launchShowDuration: 0,
-        }
-    },
+  appId: 'io.ionic.starter',
+  appName: 'reactcap',
+  webDir: 'build',
+  bundledWebRuntime: false
 };
 
 export default config;
